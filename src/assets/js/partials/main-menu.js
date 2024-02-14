8418ede38a30c618f8ee0bcb6cc76870e5043d6a
Ibrahim M. Abdalla <woxy@outlook.com>
2024-02-14 13:45:59 +0300

    feat(themes): TD-174 add custom main menu

--- src/assets/js/partials/main-menu.js
+++ src/assets/js/partials/main-menu.js
@@ -1,128 +1,160 @@
-
 class NavigationMenu extends HTMLElement {
-
     constructor() {
         super();
+        this.displayAllText = null;
+        this.brandsText = null
+        this.menus = [];
     }
 
-    #displayAllText = null
-    #menus = []
-
+    /**
+    * Fetch the menus
+    */
     connectedCallback() {
         salla.onReady(() => {
             salla.api.component.getMenus('header').then(({ data }) => {
-                return this.#menus = data
-            }).then(
-                () => {
-                    this.#displayAllText = salla.lang.get('blocks.home.display_all')
-                    this.render()
-                })
-        })
+                this.menus = data;
+                this.displayAllText = salla.lang.get('blocks.home.display_all');
+                this.brandsText = salla.lang.get('common.titles.brands')
+                this.render();
+            }).catch((error) => {
+                console.error('Error fetching menus:', error);
+            });
+        });
+    }
+
+    /** 
+    * Check if the menu has children
+    * @param {Object} menu
+    * @returns {Boolean}
+    */
+    hasChildren(menu) {
+        return menu && menu.children && menu.children.length > 0;
+    }
+
+    /**
+    * Check if the menu has products
+    * @param {Object} menu
+    * @returns {Boolean}
+    */
+    hasProducts(menu) {
+        return menu && menu.products && menu.products.length > 0;
     }
 
 
-     #hasChildren(menu = { children: [] })
-     {
-        return menu?.children?.length > 0
-     }
-
-    #hasProducts(menu = { products: [] })
-    {
-        return menu?.products?.length > 0
+    /** could be removed in the future
+    * this is a fallback in case brands title is not set in the brands settings in merchant dashboard
+    * @param {Object} menu
+    * @returns {string}
+    */
+    getMenuTitle(menu) {
+        if (menu.title) { return menu.title }
+        if (menu.id === 'brands') { return this.brandsText }
     }
 
-    #clx(classes)
-    {
-     return `"${classes}"`
+    /**
+    * Get the classes for desktop menu
+    * @param {Object} menu
+    * @param {Boolean} isRootMenu
+    * @returns {String}
+    */
+    getDesktopClasses(menu, isRootMenu) {
+        return `!hidden lg:!block ${isRootMenu ? 'root-level lg:!inline-block' : 'relative'} ${menu.products ? ' mega-menu' : ''}
+        ${this.hasChildren(menu) ? ' has-children' : ''}`
     }
 
+    /**
+    * Get the mobile menu
+    * @param {Object} menu
+    * @param {String} displayAllText
+    * @returns {String}
+    */
+    getMobileMenu(menu, displayAllText) {
+        const menuImage = menu.image ? `<img src="${menu.image}" class="rounded-full" width="48" height="48" alt="${this.getMenuTitle(menu)}" />` : '';
 
-
-    #getDesktopClasses(menu, isRootMenu){
-        return this.#clx(`!hidden lg:!block ${isRootMenu ? 'root-level lg:!inline-block' : 'relative'} ${menu.products ? ' mega-menu' : ''}
-        ${this.#hasChildren(menu) ? ' has-children' : ''}`)
-    }
-
-
-    #getMobileMenu(menu = { children: [] }, displayAllText = '') {
-        const menuImage = menu.image ? /*html*/`<img src=${menu.image} class="rounded-full" width="48" height="48" alt=${menu.title} /> ` : ''
-
-        return /*html*/`
-        <li class="lg:hidden text-sm font-bold" ${menu.attrs}>
-             ${!this.#hasChildren(menu) ? /*html*/`
-             <a  ${menu.link_attrs} aria-label=${menu.title ? menu.title : 'category'} href=${menu.url} class=${this.#clx(`text-gray-500 ${menu.image ? '!py-3' : ''}`)}>
+        return `
+        <li class="lg:hidden text-sm font-bold">
+            ${!this.hasChildren(menu) ? `
+                <a href="${menu.url}" aria-label="${this.getMenuTitle(menu)|| 'category'}" class="text-gray-500 ${menu.image ? '!py-3' : ''}">
                     ${menuImage}
-             <span>${menu.title || ''}</span>
-             </a>`
-                :
-            /*html*/`
-            <span class=${this.#clx(menu.image ? '!py-3' : '')}>
-            ${menuImage}
-            ${menu.title}
-             </span>
-             <ul>
-             <li class="text-sm font-bold"><a ${menu.link_attrs} href=${menu.url} class="text-gray-500">${displayAllText}</a></li>
-                ${menu.children.map(subMenu => this.#getMobileMenu(subMenu, displayAllText)).join(' ')}
-            </ul>
+                    <span>${this.getMenuTitle(menu) || ''}</span>
+                </a>` :
             `
-            }   
-        </li>
-        `
-    }
-
-    #getDesktopMenu(menu = { products: [], children: [] }, isRootMenu = false) {
-        return (/*html*/`
-
-    <li class=${this.#getDesktopClasses(menu, isRootMenu)} ${menu.attrs}>
-        <a ${menu.link_attrs} href=${menu.url} aria-label=${menu.title ? menu.title : 'category'}>
-            <span>${menu.title}</span>
-        </a>
-
-        ${this.#hasChildren(menu) ? /*html*/`
-        <div class=${this.#clx(`sub-menu ${this.#hasProducts(menu) ? 'w-full left-0 flex' : 'w-56'}`)}>
-                <ul class=${this.#clx(this.#hasProducts(menu) ? 'w-56 shrink-0 m-8 rtl:ml-0 ltr:mr-0' : '')}>
-                ${menu.children.map(subMenu => this.#getDesktopMenu(subMenu, false)).join(' ')}
+                <span class="${menu.image ? '!py-3' : ''}">
+                    ${menuImage}
+                    ${this.getMenuTitle(menu)}
+                </span>
+                <ul>
+                    <li class="text-sm font-bold">
+                        <a href="${menu.url}" class="text-gray-500">${displayAllText}</a>
+                    </li>
+                    ${menu.children.map((subMenu) => this.getMobileMenu(subMenu, displayAllText)).join('')}
                 </ul>
-                ${this.#hasProducts(menu) ?  /*html*/`
-                <div class="s-menu-products-wrapper">
-                <salla-products-list
-                source="selected"
-                shadow-on-hover
-                source-value=${`[${menu.products.map(({ id }) => id).join(',')}]`}
-                />
-            </div>
-                `: ''}
-        `: ''
-
-            }
-    </li>
-
-    `)
+            `}
+        </li>`;
     }
 
-    #getMenus(menus = []) {
-        return menus.map((menu => /*html*/`
-        ${this.#getMobileMenu(menu, this.#displayAllText)}
-        ${this.#getDesktopMenu(menu, true)}
-    `)).join(' ')
+    /**
+    * Get the desktop menu
+    * @param {Object} menu
+    * @param {Boolean} isRootMenu
+    * @returns {String}
+    */
+    getDesktopMenu(menu, isRootMenu) {
+        return `
+        <li class="${this.getDesktopClasses(menu, isRootMenu)}">
+            <a href="${menu.url}" aria-label="${this.getMenuTitle(menu) || 'category'}">
+                <span>${this.getMenuTitle(menu)}</span>
+            </a>
+            ${this.hasChildren(menu) ? `
+                <div class="sub-menu ${this.hasProducts(menu) ? 'w-full left-0 flex' : 'w-56'}">
+                    <ul class="${this.hasProducts(menu) ? 'w-56 shrink-0 m-8 rtl:ml-0 ltr:mr-0' : ''}">
+                        ${menu.children.map((subMenu) => this.getDesktopMenu(subMenu, false)).join('\n')}
+                    </ul>
+                    ${this.hasProducts(menu) ? `
+                        <div class="s-menu-products-wrapper">
+                            <salla-products-list
+                                source="selected"
+                                shadow-on-hover
+                                source-value="[${menu.products.map(({ id }) => id).join(',')}]"
+                            />
+                        </div>` : ''}
+                </div>` : ''}
+        </li>`;
     }
 
-    #getHeaderMenu(menus = []) {
-
-        return /*html*/ `
-    <nav id="mobile-menu" class="mobile-menu">
-        <ul class="main-menu">
-    ${this.#getMenus(menus)}
-        </ul>
-        <button class="btn--close close-mobile-menu sicon-cancel lg:hidden"></button>
-    </nav>
-    <button class="btn--close-sm close-mobile-menu sicon-cancel hidden"></button>
-    `
+    /**
+    * Get the menus
+    * @param {Array} menus
+    * @returns {String}
+    */
+    getMenus(menus) {
+        return menus.map((menu) => `
+            ${this.getMobileMenu(menu, this.displayAllText)}
+            ${this.getDesktopMenu(menu, true)}
+        `).join('\n');
     }
 
+    /**
+    * Get the header menu
+    * @param {Array} menus
+    * @returns {String}
+    */
+    getHeaderMenu(menus) {
+        return `
+        <nav id="mobile-menu" class="mobile-menu">
+            <ul class="main-menu">
+                ${this.getMenus(menus)}
+            </ul>
+            <button class="btn--close close-mobile-menu sicon-cancel lg:hidden"></button>
+        </nav>
+        <button class="btn--close-sm close-mobile-menu sicon-cancel hidden"></button>`;
+    }
 
+    /**
+    * Render the header menu
+    */
     render() {
-        this.innerHTML = this.#getHeaderMenu(this.#menus)
+        this.innerHTML = this.getHeaderMenu(this.menus);
     }
 }
 
